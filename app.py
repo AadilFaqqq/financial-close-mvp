0a9fda30f427d9ae42e2bc6a29d4cacb64afadd9
AadilFaqqq <aadilnuriya1@gmail.com>
2026-09-02 19:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ st.set_page_config(
 # ============ SIMPLE PASSWORD LOGIN ============
 password_input = st.text_input("🔐 Enter password:", type="password")
 valid_passwords = ["aadil-admin-forever", "trial-30days"]
-if password_input != "trial-30days":
-if password_input != "aadil-admin-forever":
+if password_input != "aadil-admin-forever, trial-30days":
     st.error("❌ Invalid password")
     st.stop()
 
